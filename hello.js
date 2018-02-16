b9b1bb172dcb8f446f997004c6ab9eb864058eb2
Ganesh V <ganesh-pt1936@csez.zohocorpin.com>
2018-02-16 19:42:20 +0530

    Cpp called from Node and displayed in web

---
+++ hello.js
@@ -0,0 +1,26 @@
+// hello.js
+const addon = require('./build/Release/addon');
+
+var http = require("http");
+
+http.createServer(function (request, response) {
+
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/html'})
+   //response.write(request.url);
+   // Send the response body as "Hello World"
+   if(request.url === '/zoho')
+	   response.write('Ganesh and nandu at zoho')
+   else
+	   response.write('Ganesh and nandu at Any company');
+   response.write(addon.hello());
+   response.end('<html><body><form><h1>Friends</h1><ul><li>Ganesh</li><li>Nandu</li></ul></body></html>\n');
+}).listen(8081);
+
+
+// Console will print the message
+console.log('Server running at	 http://127.0.0.1:8081/');
+console.log(addon.hello());
+// Prints: 'world'
