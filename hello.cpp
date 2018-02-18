7d74473321d8c46b31aa3568caacfbb043c7ef82
sirganeshv <ganeshtbk@gmail.com>
2018-02-18 23:29:44 +0530

    Basic addition

---
+++ hello.cpp
@@ -0,0 +1,43 @@
+#include <node.h>
+
+namespace demo {
+
+using v8::FunctionCallbackInfo;
+using v8::Isolate;
+using v8::Local;
+using v8::Object;
+using v8::String;
+using v8::Number;
+using v8::Value;
+
+void Method(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
+}
+
+/*void Test(const FunctionCallbackInfo<Value>& args) {
+	int32_t a = 3;
+	int32_t b = 4;
+	Isolate* isolate = args.GetIsolate();
+	Local<Number> num = Number::New(isolate, (a+b));
+    args.GetReturnValue().Set(num);
+}*/
+	
+ void add(const FunctionCallbackInfo<Value>& args) {
+        Isolate* isolate = args.GetIsolate();
+
+
+        double value = args[0]->NumberValue() + args[1]->NumberValue();
+        Local<Number> num = Number::New(isolate, value);
+
+        args.GetReturnValue().Set(num);
+    }
+void init(Local<Object> exports) {
+  NODE_SET_METHOD(exports, "hello", Method);
+  //NODE_SET_METHOD(exports, "test", Test);
+	NODE_SET_METHOD(exports,"add",add);
+}
+
+NODE_MODULE(demo, init)
+
+}  // namespace demo
--- hello.js
+++ hello.js
@@ -1,4 +1,3 @@
-// hello.js
 const addon = require('./build/Release/addon');
 
 var http = require("http");
@@ -11,11 +10,12 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/html'})
    //response.write(request.url);
    // Send the response body as "Hello World"
+   response.write(addon.hello());
+   response.write((addon.add(3,5)).toString());
    if(request.url === '/zoho')
 	   response.write('Ganesh and nandu at zoho')
    else
 	   response.write('Ganesh and nandu at Any company');
-   response.write(addon.hello());
    response.end('<html><body><form><h1>Friends</h1><ul><li>Ganesh</li><li>Nandu</li></ul></body></html>\n');
 }).listen(8081);
 
