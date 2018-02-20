8e9ca06143c43c91cadefa60f9484de60f678741
Ganesh V <ganesh-pt1936@csez.zohocorpin.com>
2018-02-20 10:27:40 +0530

    Spacings taken care

--- web.cpp
+++ web.cpp
@@ -15,7 +15,7 @@ using v8::Value;
 void printname(const FunctionCallbackInfo<Value>& args) {
 	//Isolate* isolate = args.GetIsolate();
 	//double value = args[0]->NumberValue() + args[1]->NumberValue();
-    //Local<Number> num = Number::New(isolate, value);
+	//Local<Number> num = Number::New(isolate, value);
 	//args.GetReturnValue().Set(num);
 	//args[0]->StringValue();
 	v8::String::Utf8Value param1(args[0]->ToString());
@@ -25,11 +25,10 @@ void printname(const FunctionCallbackInfo<Value>& args) {
 	Isolate* isolate = args.GetIsolate();
 	std::cout<<"hello";
 	//args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
-
 }
 
 void init(Local<Object> exports) {
-  NODE_SET_METHOD(exports, "printname", printname);
+	NODE_SET_METHOD(exports, "printname", printname);
 }
 
 NODE_MODULE(demo1, init)
