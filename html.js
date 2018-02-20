cc9ddd425a69c9f390078bb6392c40376e3b5dc0
Ganesh V <ganesh-pt1936@csez.zohocorpin.com>
2018-02-20 12:03:14 +0530

    Code cleanup

--- html.js
+++ html.js
@@ -13,23 +13,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 permittedLinker = ['localhost:8080', '127.0.0.1:8080'];  // who can link here?
 
 app.use(function(req, res, next) {
-  var i=0, notFound=1, referer=req.get('Referer');
-
-  if ((req.path==='/') || (req.path==='')) return next(); // pass calls to '/' always
-
-  if (referer){
-      while ((i<permittedLinker.length) && notFound){
-      notFound= (referer.indexOf(permittedLinker[i])===-1);
-      i++;
-      }
-  }
-
-  if (notFound) { 
-	res.redirect("/");
-  }
-  else {
-    next(); // access is permitted, go to the next step in the ordinary routing
-  }
+	var i=0, notFound=1, referer=req.get('Referer');
+	if ((req.path==='/') || (req.path==='')) return next(); // pass calls to '/' always
+	if (referer){
+		while ((i<permittedLinker.length) && notFound){
+			notFound= (referer.indexOf(permittedLinker[i])===-1);
+			i++;
+		}
+	}
+	if (notFound) { 
+		res.redirect("/");
+	}
+	else {
+		next(); // access is permitted, go to the next step in the ordinary routing
+	}
 });
 
 app.get('/',function(req,resp) {
@@ -47,11 +44,12 @@ app.get('/',function(req,resp) {
 	});
 });
 
-app.listen(8080, function() {
-	app.post('/myaction', function(req, res) {
+app.post('/myaction', function(req, res) {
 	webaddon.printname(req.body.name);
 	res.write('You sent the name "' + req.body.name + '".');
 	res.end();
 });
+
+app.listen(8080, function() {
 	console.log('Server running at http://127.0.0.1:8080/');
 });
