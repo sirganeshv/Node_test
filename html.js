ced4f520b983fb6211f8b946e86c18ae680c9103
Ganesh V <ganesh-pt1936@csez.zohocorpin.com>
2018-02-20 10:27:57 +0530

    Home url changed from /form to /

--- html.js
+++ html.js
@@ -12,31 +12,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/myaction', function(req, res) {
 	webaddon.printname(req.body.name);
-  res.write('You sent the name "' + req.body.name + '".');
-  res.end();
-  //webaddon.printName(req.body.name);
+	res.write('You sent the name "' + req.body.name + '".');
+	res.end();
 });
 
-app.get('/form',function(req,resp) {
-	if (req.url === "/form") {
-        fs.readFile("testhtml.html", function (error, pgResp) {
-            if (error) {
-                resp.writeHead(404);
-                resp.write('Contents you are looking are Not Found');
-            } else {
-                resp.writeHead(200, { 'Content-Type': 'text/html' });
-				resp.write("test");
-                resp.write(pgResp);
-            }
-            resp.end();
-        });
-    } else {
-        //4.
-        resp.writeHead(200, { 'Content-Type': 'text/html' });
-        resp.write('<h1>Product Manaager</h1><br /><br />To create product please enter: ' + req.url);
-        resp.end();
-    }
+app.get('/',function(req,resp) {
+	fs.readFile("testhtml.html", function (error, pgResp) {
+		if (error) {
+			resp.writeHead(404);
+			resp.write('Contents you are looking are Not Found');
+		} 
+		else {
+			resp.writeHead(200, { 'Content-Type': 'text/html' });
+			resp.write("test");
+			resp.write(pgResp);
+		}
+		resp.end();
+	});
 });
+
 app.listen(8080, function() {
-  console.log('Server running at http://127.0.0.1:8080/');
+	console.log('Server running at http://127.0.0.1:8080/');
 });
